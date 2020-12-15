68ce63ac5d899f7a7202d7d55c52c9470ea64c3b
kfiddle <kenjfiddle@gmail.com>
2020-12-15 00:29:51 -0500

    second page added

--- src/main/resources/static/currencyConverter2.js
+++ src/main/resources/static/currencyConverter2.js
@@ -1,83 +1,120 @@
 const forms = document.getElementById('forms');
-const formAmounts = [];
 const addFieldButton = document.getElementById('addField');
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const answer = document.getElementById('answer');
 
+const poundFields = [];
+const shillingsFields = [];
+const penceFields = [];
 
 addFieldButton.addEventListener('click', addAField);
-// plus.addEventListener('click', addTheFields);
-// minus.addEventListener('click', subtractTheFields);
-
+plus.addEventListener('click', addTheFields);
+minus.addEventListener('click', subtractTheFields);
 
 addAField();
 
-function addAField(){
+function addAField() {
     let formDiv = document.createElement('div');
     let poundsInput = document.createElement('input');
+    poundFields.push(poundsInput);
     let shillingsInput = document.createElement('input');
+    shillingsFields.push(shillingsInput);
     let penceInput = document.createElement('input');
+    penceFields.push(penceInput);
 
-    poundsInput.setAttribute('placeholder', 'text');
-    shillingsInput.setAttribute('placeholder', 'text');
-    penceInput.setAttribute('placeholder', 'text');
+    poundsInput.setAttribute('placeholder', 'pounds');
+    poundsInput.setAttribute('type', 'text');
+    shillingsInput.setAttribute('placeholder', 'shillings');
+    shillingsInput.setAttribute('type', 'text');
+    penceInput.setAttribute('placeholder', 'pence');
+    shillingsInput.setAttribute('type', 'text');
 
     formDiv.appendChild(poundsInput);
     formDiv.appendChild(shillingsInput);
     formDiv.appendChild(penceInput);
     forms.appendChild(formDiv);
-    let formTotal = formRender(poundsInput, shillingsInput, penceInput);
-    formAmounts.push(formTotal);
-    let testing = document.createElement('li');
-    testing.innerText = formTotal;
-    answer.appendChild(testing);
-
-}
-
-function totalPenceConvert(pounds, shillings, pence) {
-    return (pounds * 240) + (shillings * 12) + pence;
-}
-
-function formRender(poundsInput, shillingsInput, penceInput) {
-
-    let pounds = 0;
-    let shillings = 0;
-    let pence = 0;
-
-    if (poundsInput.value !== "") {
-        pounds = parseInt(poundsInput.value);
-    }
-
-    if (shillingsInput.value !== "") {
-        shillings = parseInt(shillingsInput.value);
-    }
-
-    if (penceInput.value !== "") {
-        pence = parseInt(penceInput.value);
-    }
-
-    return totalPenceConvert(pounds, shillings, pence);
 }
 
 
+function convertBackAndDisplay(totalInPence) {
+    let poundsOut = ~~(totalInPence / 240);
+    let remainingPence = totalInPence % 240;
+    let shillingsOut = ~~(remainingPence / 12);
+    let penceOut = remainingPence % 12;
+
+    let totalPence = document.createElement('li');
+    let displayPounds = document.createElement('li');
+    let displayShillings = document.createElement('li');
+    let displayPence = document.createElement('li');
+
+    totalPence.innerHTML = "total in pence : " + totalInPence;
+
+    displayPounds.innerText = "pounds : " + poundsOut;
+    displayShillings.innerText = "shillings : " + shillingsOut;
+    displayPence.innerText = "pence : " + penceOut;
+
+    answer.appendChild(displayPounds);
+    answer.appendChild(displayShillings);
+    answer.appendChild(displayPence);
+}
 
 
+function addTheFields() {
+    let totalPounds = 0;
+    let totalShillings = 0;
+    let totalPence = 0;
 
+    for (let i = 0; i < poundFields.length; i++) {
 
+        if (poundFields[i].value !== "") {
+            totalPounds += parseInt(poundFields[i].value);
+        }
+        if (shillingsFields[i].value !== "") {
+            totalShillings += parseInt(shillingsFields[i].value);
+        }
+        if (penceFields[i].value !== "") {
+            totalPence += parseInt(penceFields[i].value);
+        }
+    }
+    convertBackAndDisplay((totalPounds * 240) + (totalShillings * 12) + totalPence);
+}
 
-// function addTheFields(){
-//     let total = 0;
-//     for (let i = 0; i < formAmounts.length; i++){
-//         total += formAmounts[i];
-//     }
-//     let ourResult = document.createElement('li');
-//     let testing = document.createElement('li');
-//     testing.innerText = formAmounts.length;
-//     ourResult.innerText = total;
-//     answer.appendChild(ourResult);
-//     answer.appendChild(testing);
-// }
+function subtractTheFields() {
+    let minusPounds = parseInt(poundFields[0].value);
+    let minusShillings = parseInt(shillingsFields[0].value);
+    let minusPence = parseInt(penceFields[0].value);
+
+    if (poundFields[0] === "") {
+        minusPounds = 0;
+    }
+    if (shillingsFields[0] === "") {
+        minusShillings = 0;
+    }
+    if (penceFields[0] === "") {
+        minusPence = 0;
+    }
+
+    for (let i = 1; i < poundFields.length; i++) {
+        if (poundFields[i].value === "") {
+            poundFields[i] = 0;
+        }
+        minusPounds = minusPounds - parseInt(poundFields[i].value);
+
+        if (shillingsFields[0] === "") {
+            shillingsFields[i] = 0;
+        }
+        minusShillings -= parseInt(shillingsFields[i].value);
+
+        if (penceFields[0] === "") {
+            penceFields[i] = 0;
+        }
+        minusPence -= parseInt(penceFields[i].value);
+
+        convertBackAndDisplay((minusPounds * 240) + (minusShillings * 12) + (minusPence));
+
+    }
+}
 
 
 
